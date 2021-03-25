f540bc56771edd7824979360e36380ef15152760
Karan Shaw <74974248+karan-penny@users.noreply.github.com>
2021-03-26 00:24:59 +0530

    first commit

--- .next/server/pages/_document.js
+++ .next/server/pages/_document.js
@@ -1462,7 +1462,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _material_ui_core_styles__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_material_ui_core_styles__WEBPACK_IMPORTED_MODULE_3__);
 /* harmony import */ var _src_theme__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../src/theme */ "./src/theme.js");
 
-var _jsxFileName = "D:\\Projects\\demo\\blogging-app\\pages\\_document.js";
+var _jsxFileName = "D:\\Projects\\demo\\movie-app\\pages\\_document.js";
 
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
 
